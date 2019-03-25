3ae80e6f75375552f394fcadfd7dc1ede19747b7
Daniel <weiand.daniel@googlemail.com>
2019-03-25 13:40:05 +0100

    saves new file with timestamp but RF stays in original workfile

--- RobofontScripts/saveCopy.py
+++ RobofontScripts/saveCopy.py
@@ -1,12 +1,22 @@
 # menuTitle : Save Copy
+
 # Creates a new .ufo in the same directory with the current date and time as name.
 import time, datetime, os
+from pathlib import Path
 
-path = os.getcwd()
+#folder = os.getcwd()
 font = CurrentFont()
+path = font.path
+folder = Path(path).parents[0]
+name = font.info.familyName
+save_font = font.copy()
 
 now = datetime.datetime.now()
-todaystr = now.strftime("%d-%m-%Y %H:%M:%S")
+today = now.strftime("%d-%m-%Y %H:%M:%S")
 
-#font.save('%s.ufo'% (todaystr))
+
+save_font.save('%s/%s_%s.ufo'%(folder, name, today))
 print (path)
+print (folder)
+print ('%s/%s_%s.ufo'%(folder, name, today))
+
