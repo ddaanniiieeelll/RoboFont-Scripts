c8a097a5668d80ed821874d861172361f5a02443
ddaanniiieeelll <weiand.daniel@googlemail.com>
2019-04-18 15:27:09 +0200

    update filenameformat

--- saveCopy.py
+++ saveCopy.py
@@ -1,22 +1,20 @@
 # menuTitle : Save Copy
-
 # Creates a new .ufo in the same directory with the current date and time as name.
 import time, datetime, os
 from pathlib import Path
 
-#folder = os.getcwd()
 font = CurrentFont()
 path = font.path
 folder = Path(path).parents[0]
-name = font.info.familyName
+fName = font.info.familyName
+sName = font.info.styleName
 save_font = font.copy()
 
 now = datetime.datetime.now()
-today = now.strftime("%d-%m-%Y %H:%M:%S")
+today = now.strftime("%d-%m-%Y %H.%M")
 
 
-save_font.save('%s/%s_%s.ufo'%(folder, name, today))
-print (path)
-print (folder)
-print ('%s/%s_%s.ufo'%(folder, name, today))
+save_font.save('%s/%s-%s_%s.ufo'%(folder, fName, sName, today))
 
+print ('saveCopy done')
+print ('Your saveCopy is here:', Path(path).parents[0])
