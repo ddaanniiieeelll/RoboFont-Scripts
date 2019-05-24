de6e2a0f4db7da4ead73dd56fc6a91d3cb32abad
ddaanniiieeelll <weiand.daniel@googlemail.com>
2019-05-24 18:18:17 +0200

    add highlightTool

--- SoftOtterTool/SoftOtterTool.py
+++ SoftOtterTool/SoftOtterTool.py
@@ -3,7 +3,10 @@
 
 from vanilla import *
 from mojo.UI import *
-from mojo.events import addObserver
+from mojo.events import addObserver, removeObserver
+from mojo.drawingTools import *
+from defconAppKit.windows.baseWindow import BaseWindowController
+
 
 ########## markingTool
 
@@ -265,6 +268,58 @@ class outlineTool(object):
     def setDirectionCallback(self, sender):
          setDirection(self.w)
 
+########## highlightTool
+
+class highlightPointsOnMetrics(BaseWindowController):
+
+    def __init__(self):
+        self.w = FloatingWindow((1800, 680, 135, 60), "Highlight Tool", minSize=(123, 200))
+
+        # a checkbox to turn the tool on/off
+        self.w.showPoints = CheckBox((10, 10, -10, 50), 'highlight points\n on metric line', value=False)
+
+        # add an observer to the drawPreview event
+        addObserver(self, "highlightPoints", "draw")
+
+        # open window
+        self.setUpBaseWindowBehavior()
+        self.w.open()
+
+    def windowCloseCallback(self, sender):
+        # remove observer when window is closed
+        removeObserver(self, 'draw')
+        super(highlightPointsOnMetrics, self).windowCloseCallback(sender)
+
+    def highlightPoints(self, info):
+        # check if checkbox is selected
+        if not self.w.showPoints.get():
+            return
+        f = CurrentFont()
+        # get the current glyph
+        glyph = info["glyph"]
+        sc = info['scale']
+
+        asc = f.info.ascender
+        xhe = f.info.xHeight
+        cap = f.info.capHeight
+        dsc = f.info.descender
+        size = 8 * sc
+        # draw highlight
+        stroke(None)
+        if glyph is not None:
+            for c in glyph:
+                for s in c:
+                    for p in s:
+                        if p.y == asc or p.y == xhe or p.y == cap or p.y == dsc or p.y == 0:
+
+                            # fill(1, 0, 0, .6)
+
+                            fill(None)
+                            stroke(0, 0.8, 0.8, .8)
+                            strokeWidth(3 * sc)
+                            rect(p.x-size/2, p.y-size/2, size, size)
+
+
 ########## SoftOtterTool in Inspector
 
 class SoftOtterTools(object):
@@ -276,6 +331,7 @@ class SoftOtterTools(object):
         self.editor = Group((0, 0, -0, -0))
         self.editor.markingToolButton = Button((10,10,-10,20), 'Marking Tool', callback=self.markingToolCallback)
         self.editor.outlineToolButton = Button((10,40,-10,20), 'Outline Tool', callback=self.outlineToolCallback)
+        self.editor.highlightToolButton = Button((10, 70, -10, 20), 'Highlight Tool', callback=self.highlightToolCallback)
 
     def inspectorWindowWillShowDescriptions(self, notification):
         # create an inspector item
@@ -289,6 +345,8 @@ class SoftOtterTools(object):
     def outlineToolCallback(self, sender):
         outlineTool()
 
+    def highlightToolCallback(self, sender):
+        highlightPointsOnMetrics()
 
 
 SoftOtterTools()
