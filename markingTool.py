97d50d7b8e75beec88526751f0b1d4f67806d45f
ddaanniiieeelll <weiand.daniel@googlemail.com>
2019-08-24 14:02:34 +0200

    rename outline and markingTool

--- markingTool.py
+++ markingTool.py
@@ -1,115 +1,159 @@
 from vanilla import *
 from mojo.UI import *
+from defconAppKit.windows.baseWindow import BaseWindowController
 
-class markGlyphs(object):
+
+class markingTool(BaseWindowController):
+
     def __init__(self):
-        # The Tool Window
-        self.w = FloatingWindow((1800, 300, 200, 195), 'Marking Tool')
-        self.w.buttonMark = Button((10, 10, -10, 20), '\u2190 mark', callback=self.toggleMarkDrawer)
-        self.w.buttonGroup = Button((10, 40, -10, 20), 'make group \u2192', callback=self.toggleGroupDrawer)
-        # mark drawer
-        self.d = Drawer((180, 160), self.w)
-        self.d.componentsAndOutlinesButton = Button((10, 10, -10, 20), 'comp. + outlines', callback=self.componentsAndOutlinesCallback)
-        self.d.componentsButton = Button((10,40,-10,20), 'components', callback=self.componentsCallback)
-        self.d.usedAsComponentButton = Button((10,70,-10,20), 'used as components', callback=self.usedAsComponentCallback)
-        self.d.markOverlapButton = Button((10,100,-10,20), 'mark overlaps', callback=self.markOverlapCallback)
-        # group drawer
-        self.g = Drawer((180, 120), self.w, preferredEdge = 'right')
-        self.g.componentsAndOutlinesGroupButton = Button((10, 10, -10, 20), 'comp. + outlines', callback=self.componentsAndOutlinesGroupCallback)
-        self.g.componentsGroupButton = Button((10, 40, -10, 20), 'components', callback=self.componentsGroupCallback)
-        # self.g.usedAsComponentsGroupButton = Button((10,70,-10,20), 'used as components', callback=self.usedAsComponentsGroupCallback)
+        self.w = FloatingWindow((1800, 550, 310, 170), 'Marking Tool')
+
+        self.w.checkBoxOverlaps = CheckBox((10,10,-10,20), 'Overlaps', callback=self.checkBoxOverlapsCallback)
+        self.w.checkBoxComponents = CheckBox((10,40,-10,20), 'Components', callback=self.checkBoxComponentsCallback)
+        self.w.checkBoxComponentsAndOutlines = CheckBox((10,70,-10,20), 'Components and Outlines', callback=self.checkBoxComponentsAndOutlinesCallback)
+        # self.w.checkBoxUsedAsComponents = CheckBox((10,100,-10,20), 'Used as components')
+
+        self.w.buttonCheck = Button((10, -30, 90, 15), 'Mark', sizeStyle = 'small', callback=self.checks)
+        self.w.buttonGroup = Button((110, -30, 90, 15), 'Group', sizeStyle = 'small', callback=self.groups)
+        self.w.buttonClose = Button((210, -30, 90, 15), 'Close', sizeStyle = 'small', callback=self.closeWindow)
 
         self.w.open()
-        # self.d.open()
-
-##### Callbacks
-    # open mark drawer
-    def toggleMarkDrawer(self, sender):
-        self.d.toggle()
-    # open group drawer
-    def toggleGroupDrawer(self, sender):
-        self.g.toggle()
-    ##### mark Callbacks
-    def componentsAndOutlinesCallback(self, sender):
-        font = CurrentFont()
-        # glyph = CurrentGlyph()
-        print('>>> Glyphs combining components and outlines:')
-        print('---------------------------------------------')
-        for glyph in font:
-            if len(glyph.contours) > 0 and len(glyph.components) > 0:
-                glyph.prepareUndo('mark components and outlines')
-                glyph.markColor = 1, 0, 0.5, 0.35
-                glyph.performUndo()
-                print(glyph.name, end = " ")
-        print('\n')
 
 
-    def componentsCallback(self,sender):
-        font = CurrentFont()
-        # glyph = CurrentGlyph()
-        print('>>> Glyphs completely made out of components:')
-        print('---------------------------------------------')
-        for glyph in font:
-            if len(glyph.contours) == 0 and len(glyph.components) > 0:
-                glyph.prepareUndo('mark components')
-                glyph.markColor = 0, 0.25, 0.5, 0.35
-                glyph.performUndo()
-                print(glyph.name, end = " ")
-        print('\n')
+    def closeWindow(self, sender):
+        self.w.close()
 
-    def usedAsComponentCallback(self, sender):
-        font = CurrentFont()
-        glyph = CurrentGlyph()
 
-        print('>>> These glyphs are used as components')
-        print('---------------------------------------')
-        for component in glyph.components:
-            baseGlyph = font[component.baseGlyph]
-            print(component.baseGlyph)
-            baseGlyph.prepareUndo('marks glyphs used as components')
-            baseGlyph.markColor = 0.5, 0, 1, 0.35
-            baseGlyph.performUndo()
-        print('\n')
 
-    def markOverlapCallback(self, sender):
-        font = CurrentFont()
-        # glyph = CurrentGlyph()
+    def checkBoxOverlapsCallback(self, sender):
+        sender.get()
 
+    def checkBoxComponentsCallback(self, sender):
+        sender.get()
+
+    def checkBoxComponentsAndOutlinesCallback(self, sender):
+        sender.get()
+
+    # def checkBoxUsedAsComponentsCallback(self, sender):
+    #     sender.get()
+
+
+
+    def checks(self, sender):
+        if self.w.checkBoxOverlaps.get():
+            self.progress = self.startProgress('looking for overlaps')
+            self.checkOverlaps(f)
+            self.progress.close()
+
+        if self.w.checkBoxComponents.get():
+            self.progress = self.startProgress('looking for components')
+            self.checkComponents(f)
+            self.progress.close()
+
+        if self.w.checkBoxComponentsAndOutlines.get():
+            self.progress =self.startProgress('looking for glyphs with components and outlines')
+            self.checkComponentsAndOutlines(f)
+            self.progress.close()
+
+        # if self.w.checkBoxUsedAsComponents.get():
+        #     self.progress = self.startProgress('looking where outline is used as component')
+        #     # self.checkUsedAsComponents(f)
+        #     self.progress.close()
+
+    def groups(self, sender):
+        if self.w.checkBoxComponents.get():
+            self.progress = self.startProgress('looking for components')
+            self.groupComponents(f)
+            self.progress.close()
+
+        if self.w.checkBoxComponentsAndOutlines.get():
+            self.progress =self.startProgress('looking for glyphs with components and outlines')
+            self.groupComponentsAndOutlines(f)
+            self.progress.close()
+
+
+        # self.w.close()
+
+    def checkOverlaps(self, f):
+        self.progress.setTickCount(len(f))
+        print()
         print('>>> Glyphs with overlaps')
         print('------------------------')
-        for glyph in font:
+        for glyph in f:
+            self.progress.update()
             compareGlyph = glyph.copy()
             compareGlyph.removeOverlap()
             if len(glyph) != len(compareGlyph) or glyph.hasOverlap():
-                print(glyph.name, end = " ")
-                glyph.prepareUndo('mark overlaps')
+                # glyph.prepareUndo('mark overlaps')
                 glyph.markColor = 0.5, 0, 0.2, 0.75
-                glyph.performUndo()
-        print('\n')
+                # glyph.performUndo()
+                print(glyph.name, end = ", ")
 
+        # f.update()
+        print()
 
-    ##### group Callbacks
-    def componentsAndOutlinesGroupCallback(self, sender):
-        contoursAndComponentsGroup = SmartSet()
-        contoursAndComponentsGroup.name = 'components and outlines'
-        contoursAndComponentsGroup.query = 'Contours > 0 and Components >0'
-        addSmartSet(contoursAndComponentsGroup)
-        updateAllSmartSets()
+    def checkComponents(self, f):
+        self.progress.setTickCount(len(f))
+        print()
+        print('>>> Glyphs completely made out of components:')
+        print('---------------------------------------------')
+        for glyph in f:
+            self.progress.update()
+            if len(glyph.contours) == 0 and len(glyph.components) > 0:
+                # glyph.prepareUndo('mark components')
+                glyph.markColor = 0, 0.25, 0.5, 0.35
+                # glyph.performUndo()
+                print(glyph.name, end = ", ")
 
-    def componentsGroupCallback(self, sender):
+        # f.update()
+        print()
+
+    def checkComponentsAndOutlines(self, f):
+        self.progress.setTickCount(len(f))
+        print()
+        print('>>> Glyphs combining components and outlines:')
+        print('---------------------------------------------')
+        for glyph in f:
+            self.progress.update()
+            if len(glyph.contours) > 0 and len(glyph.components) > 0:
+                # glyph.prepareUndo('mark components and outlines')
+                glyph.markColor = 1, 0, 0.5, 0.35
+                # glyph.performUndo()
+                print(glyph.name, end = ", ")
+
+        # f.update()
+        print()
+
+    def groupComponents(self, f):
+        self.progress.setTickCount(len(f))
         componentsGroup = SmartSet()
         componentsGroup.name = 'components'
         componentsGroup.query = 'Contours == 0 and Components > 0'
         addSmartSet(componentsGroup)
         updateAllSmartSets()
 
-    # def usedAsComponentsGroupCallback(self, sender):
-    #     usedAsComponentsGroup = SmartSet()
-    #     usedAsComponentsGroup.name = 'used as components'
-    #     usedAsComponentsGroup.query = 'Color = 0.5, 0, 1, 0.35'
-    #     addSmartSet(usedAsComponentsGroup)
-    #     updateAllSmartSets()
+    def groupComponentsAndOutlines(self, f):
+
+        self.progress.setTickCount(len(f))
+        contoursAndComponentsGroup = SmartSet()
+        contoursAndComponentsGroup.name = 'components and outlines'
+        contoursAndComponentsGroup.query = 'Contours > 0 and Components >0'
+        addSmartSet(contoursAndComponentsGroup)
+        updateAllSmartSets()
 
 
 
-markGlyphs()
+
+
+
+
+
+
+
+
+
+
+
+
+f = CurrentFont()
+markingTool()
